5f38fe8dd721bf31b7581375fbe623d1bc42701c
nunez2020 <nunez.nunez21@gmail.com>
2020-10-09 15:33:39 -0400

    tipcalc with messages

--- tipcalc.js
+++ tipcalc.js
@@ -12,18 +12,27 @@ const totalPartySize = readlineSync.questionInt("How many people are in your par
 
 const tipPercentage = readlineSync.questionFloat("What percentage will you like to pay?")
 
-
-
 // Repeat all of those things back to the user.
 
-console.log(totalAmount);
-console.log(totalPartySize);
-console.log(tipPercentage);
+console.log(`The total amount of your bill is $${totalAmount.toFixed(2)}`);
+console.log(`The total party size is ${totalPartySize}`);
+console.log(`With a tip of ${tipPercentage}%`);
 
-// The bill tip is the tipPercentage devided by 100.
+// The bill tip is the (totalAmount * tipPercentage divided by 100)
+
+let percentage = tipPercentage / 100;
+let totalAmountToTip = totalAmount * percentage;
 
 // Total bill is the bill plus the tip.
 
+let billWithTip = totalAmount + totalAmountToTip;
+
 // Get the total amount of the bill devided by the totla amount of pepole. 
 
+let totalPerPerson = billWithTip / totalPartySize;
+
 // Calculate how much everyone is paying.
+
+console.log(`The tip should be $${totalAmountToTip.toFixed(2)}`);
+console.log(`The total check should be $${billWithTip.toFixed(2)}`);
+console.log(`Each person should pay $${totalPerPerson.toFixed(2)}`);
